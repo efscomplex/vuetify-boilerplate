61a0112014222783fffc6b77fe7b004baf170187
efscomplex <efscomplex@gmail.com>
2019-11-01 09:16:43 +0100

    vuetify boilerplate + pug +sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+- pageTitle = "Awesome Boilerplate"
+v-app
+   v-app-bar(app)
+      v-toolbar-title.headline.text-uppercase= pageTitle
+      v-spacer
+   v-content
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
